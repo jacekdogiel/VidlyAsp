5455440cc19f184a3c6184f26e9abe8542941020
jacek.dogiel@gmail.com <jacek.dogiel@gmail.com>
2019-04-22 20:37:14 +0200

    Customer and Movie edit

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,9 +9,11 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        [Display(Name = "Date of birth")]
         public DateTime Birthdate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
     }
 
